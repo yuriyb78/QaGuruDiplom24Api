3ba5b7486b6ec2a6f4c1c0bcee84f0ef33cc0c2a
yuriyb78 <byur78@mail.ru>
2024-04-29 09:12:04 +0300

    create new project fot diplom wis API test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setUp () {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+
+
+}
---
+++ src/test/java/utils/GenUserData.java
@@ -0,0 +1,13 @@
+package utils;
+
+import com.github.javafaker.Faker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenUserData {
+
+    static Faker faker = new Faker();
+
+
+}
